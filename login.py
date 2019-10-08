4354d60f2259152479a7e481813aea53be3a2228
SilverLight96 <49053362+SilverLight96@users.noreply.github.com>
2019-10-08 20:56:57 +0900

    Add files via upload

--- login.py
+++ login.py
@@ -1,12 +1,16 @@
-import main
-import login
+
 import time
 import os
 
-t = open("teacher.txt", 'r',encoding='UTF-8')
-s = open("student.txt", 'r',encoding='UTF-8')
-tdata = t.read()
-sdata = s.read()
+teacher = open("teacher.txt", 'r',encoding='UTF-8')
+student = open("student.txt", 'r',encoding='UTF-8')
+while True:
+    TeacherData = teacher.readline()
+    if not TeacherData:
+        break
+    TeacherLogin=TeacherData.split(',',maxsplit=3)
+    
+#StudentData = student.read()
 
 def start():
     while True:
@@ -15,11 +19,11 @@ def start():
         print("="*50)  # 로그인 화면의 화면 제목
 
         name = input("사용자의 고유번호를 입력해주세요: ")
-        if name in tdata:
+        if name in TeacherData:
             os.system('cls')
             break
 
-        elif name in sdata:
+        elif name in StudentData:
             os.system('cls')
             break
 
@@ -28,12 +32,11 @@ def start():
             time.sleep(2)
             # 화면 지우기 추가할 것=>파이참에는 없는 기능?
             os.system('cls')
-            print(login.start()) #2번 반복하여 불러올 시 에러 생김
+            print(start())  # 2번 반복하여 불러올 시 에러 생김
 
-    print(main.screen())
-    t.close()
-    s.close()
-start()
+
+    teacher.close()
+    student.close()
 # name과 불러온 정보 일치시 화면 지운 후 메인 화면 출력 if name == rnumber.
 # 고유번호 존재하지 않을 시 오류메시지 출력 후 2초 뒤 모든 화면 삭제 및 로그인 화면 출력
 
