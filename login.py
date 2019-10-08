daacc421580c655a510fd837956baf2d4b9cbece
SilverLight96 <49053362+SilverLight96@users.noreply.github.com>
2019-10-08 16:51:20 +0900

    import 오류 수정,메인화면 종료시 로그인화면 재출력 오류, T3가 아닌 번호 입력시 로그인되는 문제

--- login.py
+++ login.py
@@ -1,37 +1,39 @@
+import main
 import login
 import time
 import os
 
-
-t = open("teacher.txt", 'r', encoding='UTF-8')
-s = open("student.txt", 'r', encoding='UTF-8')
+t = open("teacher.txt", 'r',encoding='UTF-8')
+s = open("student.txt", 'r',encoding='UTF-8')
 tdata = t.read()
 sdata = s.read()
 
-while True:
-    print("="*50)
-    print(" "*22, "LISEO")
-    print("="*50)  # 로그인 화면의 화면 제목
+def start():
+    while True:
+        print("="*50)
+        print(" "*22, "LISEO")
+        print("="*50)  # 로그인 화면의 화면 제목
 
-    name = input("사용자의 고유번호를 입력해주세요: ")
-    if name in tdata:
-        break
+        name = input("사용자의 고유번호를 입력해주세요: ")
+        if name in tdata:
+            os.system('cls')
+            break
 
-    elif name in sdata:
-        break
+        elif name in sdata:
+            os.system('cls')
+            break
 
-    else:
-        print("해당하는 고유번호가 없습니다. 다시 입력해 주세요.")
-        time.sleep(2)
-        # 화면 지우기 추가할 것=>파이참에는 없는 기능?
-        os.system('cls')
-        print(login) #2번 반복하여 불러올 시 에러 생김
-import main
-os.system('cls')
-print(main)
-t.close()
-s.close()
+        else:
+            print("해당하는 고유번호가 없습니다. 다시 입력해 주세요.")
+            time.sleep(2)
+            # 화면 지우기 추가할 것=>파이참에는 없는 기능?
+            os.system('cls')
+            print(login.start()) #2번 반복하여 불러올 시 에러 생김
 
+    print(main.screen())
+    t.close()
+    s.close()
+start()
 # name과 불러온 정보 일치시 화면 지운 후 메인 화면 출력 if name == rnumber.
 # 고유번호 존재하지 않을 시 오류메시지 출력 후 2초 뒤 모든 화면 삭제 및 로그인 화면 출력
 
--- main.py
+++ main.py
@@ -5,27 +5,29 @@ import time
 #import MyClass
 #import StudentCare
 
-print("="*50)
-print(" "*18,'1. 마이페이지')
-print(" "*18,'2. 강의 정보 열람')
-print(" "*18,'3. 내 강의')
-print(" "*18,'4. 학생 관리')
-print(" "*18,'5. 종료')
-print("="*50)
+def screen():
+    print("="*50)
+    print(" "*18,'1. 마이페이지')
+    print(" "*18,'2. 강의 정보 열람')
+    print(" "*18,'3. 내 강의')
+    print(" "*18,'4. 학생 관리')
+    print(" "*18,'5. 종료')
+    print("="*50)
 
-select=int(input("원하시는 항목을 선택해 주세요 : "))
-if select==1: #마이페이지->MyPage
-    print('you select : ', select)
-elif select==2: #강의 정보 열람->ClassView
-    print('you select : ', select)
-elif select==3: #내 강의->MyClass
-    print('you select : ', select)
-elif select==4: #학생 관리->StudentCare
-    print('you select : ', select)
-elif select==5: #종료
-    exit()
-else:
-    print("1-5사이의 항목을 선택해 주세요.")#숫자입력규칙 적용필요
-    time.sleep(2)
-    os.system('cls')
+    select=int(input("원하시는 항목을 선택해 주세요 : "))
+    if select==1: #마이페이지->MyPage
+        print('you select : ', select)
+    elif select==2: #강의 정보 열람->ClassView
+        print('you select : ', select)
+    elif select==3: #내 강의->MyClass
+        print('you select : ', select)
+    elif select==4: #학생 관리->StudentCare
+        print('you select : ', select)
+    elif select==5: #종료
+        exit()
+    else:
+        print("1-5사이의 항목을 선택해 주세요.")#숫자입력규칙 적용필요
+        time.sleep(2)
+        os.system('cls')
+        screen()
     #메인화면 출력
