5632ed65d08476da47adf62780de79ac7ac37c88
sp8190 <56158143+sp8190@users.noreply.github.com>
2019-10-10 17:01:28 +0900

    학생 관리 추가

--- textManager.py
+++ textManager.py
@@ -88,14 +88,60 @@ def readText_Room():
         RoomInfo.append(RoomLogin)
     return RoomInfo # 장소 정보 반환
 
-def writeText_Student():
-    student = open("student.txt", 'w', encoding='UTF-8')
 
-def writeText_Teacher():
+
+def Re_UserInfo():
+    student = open("student.txt", 'w', encoding='UTF-8')
     teacher = open("teacher.txt", 'w', encoding='UTF-8')
 
-def writeText_Class():
-    classroom = open("class.txt", 'w', encoding='UTF-8')
-
-def writeText_Room():
+def Re_StuClass():
+    student = open("student.txt", 'w', encoding='UTF-8')
+    teacher = open("teacher.txt", 'w', encoding='UTF-8')
+    Class = open("class.txt", 'w', encoding='UTF-8')
     room = open("room.txt", 'w', encoding='UTF-8')
+
+
+def Re_TeaClass():
+    student = open("student.txt", 'w', encoding='UTF-8')
+    teacher = open("teacher.txt", 'w', encoding='UTF-8')
+    Class = open("class.txt", 'w', encoding='UTF-8')
+    room = open("room.txt", 'w', encoding='UTF-8')
+
+
+def Re_Student(number,content):
+    student = readText_Student()
+    if number == 1: # 이쪽은 학생 정보 삭제 content = 고유번호
+        # 몇번째줄 지워야 되는지 체크
+        i = 0
+        re_line = -1 # re_line 값이 없을 수도 있음
+        while i < len(student):
+            if content == student[i][0]:  # 맞는 고유번호인지 확인
+                re_line = i  # 몇번 째줄 지워야 하는지 확인
+                break
+            i += 1
+        r_list = []
+        read_s = open("student.txt", 'r', encoding='UTF-8')
+        # list안에 str로 한줄 씩 받기
+        while True:
+            StudentData = read_s.readline()  # student.txt 파일 읽기
+            r_list.append(StudentData) #list에 str 형태로 저장
+            if not StudentData:  # txt 마지막 줄에 도달하면 break
+                break
+        del r_list[re_line] # re_line 값이 없을 수도 있음/ 해당 줄 삭제
+        # txt 초기화 후 다시 쓰기
+        write_s = open("student.txt", 'w', encoding='UTF-8') # student.txt 초기화
+        i = 0
+        while i < len(r_list):
+            write_s.write(str(r_list[i])) # student.txt 쓰기
+            i += 1
+
+
+    else: #이쪽은 학생 정보 추가
+        re = open("student.txt", 'a', encoding='UTF-8')  # txt 문장 추가
+        aa = len(student) + 1 # 고유 번호 지정
+        bb = 'S' + str(aa) # 고유 번호 생성
+        cc = '\n'+ bb + ',' + number +','+ content # 한 문장으로 합치기 number = 이름 content = 전화번호
+        re.write(cc) # 글 쓰기
+
+
+
