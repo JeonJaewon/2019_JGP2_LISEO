3140a18967a092298f7e47d74ca4f40237088bfb
JeonJaewon <wksk04515@naver.com>
2019-10-12 23:36:25 +0900

    강의삭제 deleteClassText 완성, 수강신청,취소 enrollOrCancelClass 작업중

--- textManager.py
+++ textManager.py
@@ -289,5 +289,31 @@ def Re_Student(number,content):
         addstudent.write(newline) # 글 쓰기
 
 ########## myClass.py 전용 함수들 ##########
-def modify_Class(code, flag):   # flag : 0(추가), 1(삭제)
-    add = open("class.txt", 'a+', encoding='UTF-8-SIG')
+def deleteClassText(code):   # flag : 0(추가), 1(학생 삭제), 2(강의 삭제)
+    classFile = open("classTest.txt", 'r', encoding='UTF-8-SIG')
+    classLines = classFile.readlines() # 라인 전부 읽어오고
+    if len(classLines) != 0:
+        classLines[0] = classLines[0].replace(u"\ufeff", '')  # utf-8-sig로도 안없어져서 강제로 없앰
+    classWrite = open("classTest.txt", 'w', encoding='UTF-8-SIG') # W로 오픈하면 텍스트 전부 삭제됨.
+
+    for line in classLines:
+        if code != line.split(' ')[0]: # code랑 저장해놨던 line들의 코드랑 비교해서
+            classWrite.write(line) # 같지 않으면, 즉 삭제할려고 했던게 아니면 다시 써준다
+
+def enrollOrCancelClass(classCode,studentCode, flag):  # flag : 0(수강 신청), 1(수강 취소)
+    classFile = open("classTest.txt", 'r', encoding='UTF-8-SIG')
+    classLines = classFile.readlines()
+    if len(classLines) != 0:
+        classLines[0] = classLines[0].replace(u"\ufeff", '')  # utf-8-sig로도 안없어져서 강제로 없앰
+    classWrite = open("classTest.txt", 'w', encoding='UTF-8-SIG')
+
+
+
+    if flag == 0:
+        for line in classLines:
+            if classCode != line.split(' ')[0]:
+                classWrite.write(line)
+    elif flag == 1:
+        for line in classLines:
+            if classCode!= line.split(' ')[0]:
+                classWrite.write(line)
